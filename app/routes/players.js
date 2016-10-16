b3008ebe9cd55327d994d21ffdf00b0c76d32653
Danny <dannyb7878@gmail.com>
2016-10-16 08:34:23 +0300

    sync

--- app/routes/players.js
+++ app/routes/players.js
@@ -4,13 +4,17 @@ const DEFAULT_SAMPLE = 5;
 
 var express = require('express');
 
+var validate = require("validate.js");
+
 var router = express.Router();
 
 var player_repo = require('../../db/repository/player_repository');
 
 router.get('/random_sample', function (req, res, next) {
 
-	let size = req.query.size || DEFAULT_SAMPLE;
+	let size = Number(req.query.size) || DEFAULT_SAMPLE;
+
+	validate.
 
 	player_repo.randomSample(size).then(function (randomPlayer) {
 		res.json(randomPlayer);
